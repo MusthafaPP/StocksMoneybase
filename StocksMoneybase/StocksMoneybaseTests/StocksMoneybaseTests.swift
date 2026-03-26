e5127adf7e0b029ffa624d5263e2f1dec7ec7cb0
Muhammed Musthafa <mmusu4u@gmail.com>
2026-03-26 22:08:57 +0400

    Created  Project StocksMoneybase

---
+++ StocksMoneybase/StocksMoneybaseTests/StocksMoneybaseTests.swift
@@ -0,0 +1,16 @@
+//
+//  StocksMoneybaseTests.swift
+//  StocksMoneybaseTests
+//
+//  Created by Muhammed Musthafa on 26/03/2026.
+//
+
+import Testing
+
+struct StocksMoneybaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
